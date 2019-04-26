b5339b6abd314e4d079f818bbd61cec2b33a3dbe
zhuimenran <2161434564@qq.com>
2019-04-26 13:11:13 +0800

    add Queue.java interface

---
+++ src/com/txb/queue/util/Queue.java
@@ -0,0 +1,13 @@
+package com.txb.queue.util;
+
+public interface Queue<E> {
+		public int getSize();
+		
+		public boolean  isEmpty();
+		
+		public void enqueue(E e);
+		
+		public E dequeue();
+		
+		public E  getFront();
+}
